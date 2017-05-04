5f681b119e472ee2cd60c70b76d9c18cdcf45770
Szymon <miks.szymon@gmail.com>
2017-05-04 19:33:50 +0200

    start project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Tablice-haszowane.cpp
+// Author      : Szymon Miks
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Tablice Haszowane" << endl; // prints Tablice Haszowane
+	return 0;
+}
